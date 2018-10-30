18b4fedcfa604f3987c6a565f335f5deae42f9dd
Ubuntu <ubuntu@ip-10-0-2-225.ap-northeast-1.compute.internal>
2018-10-30 14:59:00 +0000

    add final files

---
+++ Final/readLog.py
@@ -0,0 +1,44 @@
+import csv
+
+nameListFile = open('nameList.txt')
+lines = nameListFile.readlines()
+
+logFileList = []
+for line in lines :
+	logFile = line.replace('\n', '')
+	logFileList.append(logFile)
+
+interactionDic = dict()
+signalsDic = dict()
+rateDic = dict()
+
+for i in range(0, len(logFileList)) :
+	logFileName = logFileList[i]
+	file = open(logFileName)
+	lines = file.readlines()
+
+	for line in lines :
+		dataArr = line.split(',')
+		interaction = float(dataArr[0])
+		signals = float(dataArr[1])
+		rate = float(dataArr[2])
+
+		interactionDic[str(i + 1)] = interaction
+		signalsDic[str(i + 1)] = signals
+		rateDic[str(i + 1)] = rate
+
+
+	
+
+
+with open('numOfDevicesLog.csv', 'w') as csvfile :
+	fields = ['Number of devices', 'Interaction', 'Signals', 'Successful rate']
+	
+	writer = csv.DictWriter(csvfile, fieldnames=fields)
+	writer.writeheader()
+
+	for numOfDevices in range(1, 8) :
+		interaction = interactionDic[str(numOfDevices)]
+		signals = signalsDic[str(numOfDevices)]
+		rate = rateDic[str(numOfDevices)]
+		writer.writerow({'Number of devices': str(numOfDevices), 'Interaction' : str(interaction), 'Signals' : str(signals), 'Successful rate' : str(rate)})
